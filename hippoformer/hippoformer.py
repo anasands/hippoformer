cfe86966078d1956e99deff3ec28ccc49c0cbf93
lucidrains <lucidrains@gmail.com>
2025-10-21 07:42:51 -0700

    need assoc scan

--- hippoformer/hippoformer.py
+++ hippoformer/hippoformer.py
@@ -5,6 +5,8 @@ from einops import rearrange
 
 from x_mlps_pytorch import MLP
 
+from assoc_scan import AssocScan
+
 # helpers
 
 def exists(v):
@@ -13,3 +15,15 @@ def exists(v):
 def default(v, d):
     return v if exists(v) else d
 
+class mmTEM(Module):
+    def __init__(
+        self,
+        dim
+    ):
+        super().__init__()
+
+    def forward(
+        self,
+        data
+    ):
+        raise NotImplementedError
