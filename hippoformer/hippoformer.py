c6d1912aa4cae4e6e88491aea205f6628daaaf65
lucidrains <lucidrains@gmail.com>
2025-10-23 15:25:33 -0700

    redo rnn logic

--- hippoformer/hippoformer.py
+++ hippoformer/hippoformer.py
@@ -1,9 +1,12 @@
-import torch
-from torch import nn, stack
-from torch.nn import Module
-import torch.nn.functional as F
+from __future__ import annotations
 
-from einops import einsum, repeat, rearrange
+import torch
+from torch import nn, Tensor, stack, einsum
+import torch.nn.functional as F
+from torch.nn import Module
+from torch.jit import ScriptModule, script_method
+
+from einops import repeat, rearrange
 from einops.layers.torch import Rearrange
 
 from x_mlps_pytorch import create_mlp
@@ -22,6 +25,40 @@ def l2norm(t):
     return F.normalize(t, dim = -1)
 
 # path integration
+
+class RNN(ScriptModule):
+    def __init__(
+        self,
+        dim,
+    ):
+        super().__init__()
+        self.init_hidden = nn.Parameter(torch.randn(1, dim) * 1e-2)
+
+    @script_method
+    def forward(
+        self,
+        transitions: Tensor,
+        hidden: Tensor | None = None
+    ) -> Tensor:
+
+        batch, seq_len = transitions.shape[:2]
+
+        if hidden is None:
+            hidden = l2norm(self.init_hidden)
+            hidden = hidden.expand(batch, -1)
+
+        hiddens: list[Tensor] = []
+
+        for i in range(seq_len):
+            transition = transitions[:, i]
+
+            hidden = einsum('b i, b i j -> b j', hidden, transition)
+            hidden = F.relu(hidden)
+            hidden = l2norm(hidden)
+
+            hiddens.append(hidden)
+
+        return stack(hiddens, dim = 1)
 
 class PathIntegration(Module):
     def __init__(
@@ -45,6 +82,8 @@ class PathIntegration(Module):
 
         self.mlp_out_to_weights = Rearrange('... (i j) -> ... i j', j = dim_structure)
 
+        self.rnn = RNN(dim_structure)
+
     def forward(
         self,
         actions # (b n d)
@@ -54,22 +93,7 @@ class PathIntegration(Module):
         transitions = self.to_transitions(actions)
         transitions = self.mlp_out_to_weights(transitions)
 
-        # do it the slow way for now
-
-        structure_out = []
-
-        structure = l2norm(self.init_structure)
-        structure = repeat(structure, 'd -> b d', b = batch)
-
-        for transition in transitions.unbind(dim = 1):
-
-            structure = einsum(structure, transition, 'b i, b i j -> b j')
-            structure = F.relu(structure)
-            structure = l2norm(structure)
-
-            structure_out.append(structure)
-
-        return stack(structure_out, dim = 1)
+        return self.rnn(transitions)
 
 # proposed mmTEM
 
